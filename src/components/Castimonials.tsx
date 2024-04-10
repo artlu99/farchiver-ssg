7bacf1adc4e99808b62e5c8c5c16842021461942
artlu99 <alu99.sm@gmail.com>
2024-04-10 11:18:58 -0400

    client-side FarcasterEmbed fixes CORS issue

--- src/components/Castimonials.tsx
+++ src/components/Castimonials.tsx
@@ -1,30 +1,19 @@
 import 'react-farcaster-embed/dist/styles.css'
-import { FarcasterEmbed } from 'react-farcaster-embed' // include default styles or write your own
-
-import { useEffect, useState } from 'preact/hooks'
-import UserCount from 'components/UserCount'
+import { FarcasterEmbed } from 'react-farcaster-embed/dist/client'
 
 export default function () {
-  const [data1, setData1] = useState(null)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await FarcasterEmbed({
-        url: 'https://warpcast.com/antimofm.eth/0x724cbf',
-      })
-      setData1(response)
-    }
-
-    fetchData().catch(() => {})
-  }, [])
-
+  const username='antimofm.eth';
+  const hash='https://warpcast.com/antimofm.eth/0x724cbf';
+        
   return (
     <div className="hero h-full bg-base-100">
       <div className="hero-content min-h-screen text-center text-content">
         <div className="prose text-left">
           <h3>The easiest way to save individual casts</h3>
           <h4>Permanence provided by Pinata 🤝 IPFS</h4>
-          <div>{data1}</div>
+          <div>
+            <FarcasterEmbed username={username} hash={hash} />
+          </div>
           <h3>WIP 👀</h3>
         </div>
       </div>
