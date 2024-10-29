0df6b3421c0506e275246f919736f1c6157e7832
@artlu99 <90869437+artlu99@users.noreply.github.com>
2024-10-29 16:23:48 -0400

    Update Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -5,7 +5,7 @@ export default function () {
     <div className="prose text-left">
       <h3>Farchiver Subscription</h3>
       <h4>4 snapshots per calendar year</h4>
-      <h4>Price: 0.03 ETH (or equiv in USDC) </h4>
+      <h4>Price: 6.9 ETH (or equiv in USDC) </h4>
       <h4>Turnaround: ~24hrs</h4>
       <h3>
         <UserCount />
